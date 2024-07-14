52da42a56448ba84ae4195c7e0b29d0a5e33201e
Hasantha malshan <hasanthamalshan07@gmail.com>
2024-07-14 13:55:54 -0400

    populate spinners

--- app/src/main/java/com/example/tempconverter/MainActivity.kt
+++ app/src/main/java/com/example/tempconverter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tempconverter
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -11,5 +13,18 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Temperature Converter"
+
+        val temperatureUnits = arrayOf("Celsius", "Fahrenheit")
+
+        val fromUnitSpinner: Spinner = findViewById(R.id.fromUnit)
+        val toUnitSpinner: Spinner = findViewById(R.id.toUnit)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, temperatureUnits)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        fromUnitSpinner.adapter = adapter
+        toUnitSpinner.adapter = adapter
+
+
     }
 }
